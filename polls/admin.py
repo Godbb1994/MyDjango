1e7a953c64916d97bb443a43db6ae4eb6a29a71a
Godbb1994 <biglifeenjoy@gmail.com>
2024-03-24 23:28:24 +0800

    fix check box and background

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,4 @@
+# admin.py start
 from django.contrib import admin
 from .models import Choice, Question
 
@@ -5,17 +6,52 @@ from .models import Choice, Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
+    # 新增順序欄位
+    list_display = ("question_text", "pub_date", "was_published_recently", "type", "position")
+
+    # 新增排序功能
+    ordering = ("position",)
+
+    # 新增變更順序的動作
+    def change_position(self, request, queryset):
+        # 取得所有選取的問題
+        questions = queryset.all()
+
+        # 根據問題的 ID 對問題進行排序
+        questions = sorted(questions, key=lambda question: question.id)
+
+        # 更新問題的順序
+        for i, question in enumerate(questions):
+            question.position = i
+            question.save()
+
+        # 顯示成功訊息
+        self.message_user(request, "問題順序已更新成功。")
+
+    # 新增動作
+    actions = ["change_position"]
+
+    # 新增只讀欄位
+    readonly_fields = ("position",)
+
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
         ("Type", {"fields": ("type",)}),
-
     ]
     inlines = [ChoiceInline]
-    list_display = ["question_text", "pub_date", "was_published_recently","type"]
+
+
+
+
+
 
 admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
+
+# admin.py end
--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,7 @@
+# models.py start
 from django.db import models
 from django.utils import timezone
-from datetime import datetime, timedelta  # 同时导入 datetime 和 timedelta
-
-
+from datetime import datetime, timedelta
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
@@ -15,6 +14,11 @@ class Question(models.Model):
         now = timezone.now()
         return now - timezone.timedelta(days=1) <= self.pub_date <= now
 
+    # 新增順序欄位
+    position = models.IntegerField(default=0, null=True)
+
+
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -23,3 +27,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+# models.py end
